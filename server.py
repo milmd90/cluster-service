e2e7bccd8e287843bb46115712212ea9c8e74e2b
Matthew Miller <mattdeanmiller@gmail.com>
2019-03-19 15:59:52 -0500

    change response to json

--- server.py
+++ server.py
@@ -14,4 +14,56 @@ def hello():
 @app.route("/recommend/<saleCode>")
 @cross_origin()
 def recommend(saleCode):
-    return "Hello %s!" % saleCode
+    return jsonify({
+      "products": [
+        {
+          "id": "1",
+          "name": "this product",
+          "img": "https://images-na.ssl-images-amazon.com/images/I/61OLLS-CDoL._UX679_.jpg",
+          "brand": "nike",
+          "price": 200
+        }, {
+          "id": "2",
+          "name": "that product",
+          "img": "https://res.cloudinary.com/teepublic/image/private/s---eJtvOOE--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_-72/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-397/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1507413937/production/designs/1955718_1.jpg",
+          "brand": "bike",
+          "price": 300
+        }, {
+          "id": "1",
+          "name": "this product",
+          "img": "https://images-na.ssl-images-amazon.com/images/I/61OLLS-CDoL._UX679_.jpg",
+          "brand": "nike",
+          "price": 200
+        }, {
+          "id": "2",
+          "name": "that product",
+          "img": "https://res.cloudinary.com/teepublic/image/private/s---eJtvOOE--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_-72/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-397/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1507413937/production/designs/1955718_1.jpg",
+          "brand": "bike",
+          "price": 300
+        }, {
+          "id": "1",
+          "name": "this product",
+          "img": "https://images-na.ssl-images-amazon.com/images/I/61OLLS-CDoL._UX679_.jpg",
+          "brand": "nike",
+          "price": 200
+        }, {
+          "id": "2",
+          "name": "that product",
+          "img": "https://res.cloudinary.com/teepublic/image/private/s---eJtvOOE--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_-72/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-397/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1507413937/production/designs/1955718_1.jpg",
+          "brand": "bike",
+          "price": 300
+        }, {
+          "id": "1",
+          "name": "this product",
+          "img": "https://images-na.ssl-images-amazon.com/images/I/61OLLS-CDoL._UX679_.jpg",
+          "brand": "nike",
+          "price": 200
+        }, {
+          "id": "2",
+          "name": "that product",
+          "img": "https://res.cloudinary.com/teepublic/image/private/s---eJtvOOE--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_-72/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-397/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1507413937/production/designs/1955718_1.jpg",
+          "brand": "bike",
+          "price": 300
+        }
+      ]
+    })
