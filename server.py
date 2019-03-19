d404af8858795b9f0d679de2041494700a851a2b
Matthew Miller <mattdeanmiller@gmail.com>
2019-03-19 15:49:38 -0500

    allow cors

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask("cluster-service")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
+@cross_origin()
 def hello():
     return "Hello world!"
 
 @app.route("/recommend/<saleCode>")
+@cross_origin()
 def recommend(saleCode):
     return "Hello %s!" % saleCode
